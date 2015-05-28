ba2251c7d09fb4547e41a88b59fadc2557b84f6e
svklyster <10920@iha.dk>
2015-05-28 16:52:59 +0200

    mere rapport

--- Implementation/RealTime EyeTracking/RealTime_EyeTracking/RealTime_EyeTracking/SessionHandler.py
+++ Implementation/RealTime EyeTracking/RealTime_EyeTracking/RealTime_EyeTracking/SessionHandler.py
@@ -5,7 +5,8 @@ import os
 paraNames = ["SESSIONPATH","USINGCAM","CAMNR",
                  "VIDEOPATH","NOTES","RESOLUTION",
                  "CALTYPE","LOGFILENAME","CALFILENAME",
-                 "LOADEDCALDATA","RECORDVIDEO", "RAWDATAPATH"]
+                 "LOADEDCALDATA","RECORDVIDEO", "RAWDATAPATH",
+                 "VARIABLENAMES", "VARIABLEVALUES"]
 
 class SessionData:
 
@@ -23,6 +24,8 @@ class SessionData:
         self.calfile = None
         self.recordvideo = False
         self.rawdatapath = None
+        self.variablenames = [None, None, None, None, None, None, None, None, None, None]
+        self.variablevalues = [None, None, None, None, None, None, None, None, None, None]
         
 
     
@@ -44,14 +47,16 @@ class SessionData:
             sessionStr += "VIDEOPATH None" + os.linesep
         else:
             sessionStr +=  "VIDEOPATH " + self.videopath + os.linesep 
-        sessionStr += "NOTES " + self.notes
-        sessionStr += "RESOLUTION " + self.resolution
-        sessionStr += "CALTYPE " + self.caltype
-        sessionStr += "LOGFILENAME " + self.logfilename
-        sessionStr += "CALFILENAME " + self.calfilename
+        sessionStr += "NOTES " + self.notes + os.linesep 
+        sessionStr += "RESOLUTION " + self.resolution + os.linesep 
+        sessionStr += "CALTYPE " + self.caltype + os.linesep 
+        sessionStr += "LOGFILENAME " + self.logfilename + os.linesep 
+        sessionStr += "CALFILENAME " + self.calfilename + os.linesep 
         sessionStr += "LOADEDCALDATA " + str(self.calfile) + os.linesep
         sessionStr += "RECORDVIDEO " + str(self.recordvideo) + os.linesep
-        sessionStr += "RAWDATAPATH " + str(self.rawdatapath)
+        sessionStr += "RAWDATAPATH " + str(self.rawdatapath) + os.linesep 
+        sessionStr += "VARIABLENAMES " + str(self.variablenames) + os.linesep 
+        sessionStr += "VARIABLEVALUES " + str(self.variablevalues) + os.linesep 
         try:
             file.write(sessionStr)    
             return "fileUpdated"
@@ -69,14 +74,16 @@ class SessionData:
             sessionStr += "VIDEOPATH None" + os.linesep
         else:
             sessionStr +=  "VIDEOPATH " + str(self.videopath) + os.linesep
-        sessionStr += "NOTES " + str(self.notes)
-        sessionStr += "RESOLUTION " + str(self.resolution)
-        sessionStr += "CALTYPE " + str(self.caltype)
-        sessionStr += "LOGFILENAME " + str(self.logfilename)
-        sessionStr += "CALFILENAME " + str(self.calfilename)
+        sessionStr += "NOTES " + str(self.notes) + os.linesep 
+        sessionStr += "RESOLUTION " + str(self.resolution) + os.linesep 
+        sessionStr += "CALTYPE " + str(self.caltype) + os.linesep 
+        sessionStr += "LOGFILENAME " + str(self.logfilename) + os.linesep 
+        sessionStr += "CALFILENAME " + str(self.calfilename) + os.linesep 
         sessionStr += "LOADEDCALDATA " + str(self.calfile) + os.linesep
         sessionStr += "RECORDVIDEO " + str(self.recordvideo) + os.linesep
-        sessionStr += "RAWDATAPATH " + str(self.rawdatapath)
+        sessionStr += "RAWDATAPATH " + str(self.rawdatapath) + os.linesep 
+        sessionStr += "VARIABLENAMES " + str(self.variablenames) + os.linesep 
+        sessionStr += "VARIABLEVALUES " + str(self.variablevalues) + os.linesep 
         try:
             file.write(sessionStr)
             return "fileCreated"
@@ -117,4 +124,6 @@ def LoadPreferencesFromFile(filepath):
     newPrefData.calfile = file.readline()[14: ]
     newPrefData.recordvideo = file.readline()[12: ]
     newPrefData.rawdatapath = file.readline()[12: ]
+    newPrefData.variablenames = file.readline()[14: ]
+    newPrefData.variablevalues = file.readline()[15: ]
     return newPrefData
