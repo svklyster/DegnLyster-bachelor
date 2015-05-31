fe06ae02bf0efe6d788c5e29975548432dd3481e
svklyster <10920@iha.dk>
2015-05-31 13:46:01 +0200

    mere smukt til implementering

---
+++ Implementation/RealTime EyeTracking/RealTime_EyeTracking/tomalgoritme.py
@@ -0,0 +1,12 @@
+import numpy as np
+import EyeTracking as et
+
+#Her tilføjes diverse python-biblioteker
+
+def Track(frame, calibration_data, variables):
+
+    #Egen kode her 
+    
+    et.PackWithTimestamp(screen_point, trigger_value)
+    et.LastRunInfo(variables)
+    return
